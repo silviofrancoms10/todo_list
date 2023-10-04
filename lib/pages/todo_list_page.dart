5e57c5c98828a883eee7395f947e406e593a5a37
silviofrancoms <silviofrancoms@outlook.com>
2023-10-04 19:11:31 -0400

    Aula 92 - Personalizando o botao

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -16,15 +16,35 @@ class TodoListPage extends StatelessWidget {
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Adicione uma tarefa',
-                    hintText: 'Ex. Estudar Flutter',
+                    hintText: 'Ex. Fazer compras',
                   ),
                 ),
               ),
               SizedBox(width: 8),
               ElevatedButton(
-                onPressed: (){},
-                child: Text('+'),
-              )
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xffA435F0),
+                      padding: EdgeInsets.all(16), // EdgeInsets.zero,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusDirectional.horizontal(
+                              start: Radius.circular(8),
+                              end: Radius.circular(8)))
+                      //fixedSize: Size(100, 200),
+                      ),
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 30,
+                  )
+                  //Text(
+                  // '+',
+                  // style: TextStyle(
+                  // color: Colors.white,
+                  // fontSize: 40
+                  // ),
+                  // ),
+                  )
             ],
           ),
         ),
