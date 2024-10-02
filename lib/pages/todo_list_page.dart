f4ed447980e75a96618b7471a67041f6170020b2
silviofranco <silviofrancoms@gmail.com>
2024-10-01 22:03:42 -0400

    Adicionando Coluna para colocar outra row na tela, separando espaços entre elas com o SizedBox

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -13,39 +13,68 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Adicione uma tarefa',
-                    hintText: 'Ex. Estudar Flutter',
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                        hintText: 'Ex. Estudar Flutter',
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    /*child: Text(
+                      "+",
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),*/
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      backgroundColor: Color(0xff00d7f3),
+                      foregroundColor: Colors.white,
+                      padding: EdgeInsets.all(12),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  )
+                ],
               ),
-              SizedBox(width: 8),
-              ElevatedButton(
-                onPressed: () {},
-                /*child: Text(
-                  "+",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
+              SizedBox(height: 16,),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Você possui 0 tarefas pendentes",
+                    ),
                   ),
-                ),*/
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  backgroundColor: Color(0xff00d7f3),
-                  foregroundColor: Colors.white,
-                  padding: EdgeInsets.all(12),
-                ),
-                child: Icon(
-                  Icons.add,
-                  size: 30,
-                ),
-              )
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Limpar tudo"),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      backgroundColor: Color(0xff00d7f3),
+                      foregroundColor: Colors.white,
+                      padding: EdgeInsets.all(14),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
